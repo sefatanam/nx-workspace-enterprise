df3ff902db5e7c3328db4e881b4d02931161460e
Sefat Anam <37630292+sefatanam@users.noreply.github.com>
2022-01-22 18:26:18 +0600

    got button

--- apps/enterprise-angular/src/app/app.module.ts
+++ apps/enterprise-angular/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { HttpClientModule } from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {NxWelcomeComponent} from './nx-welcome.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ComponentsModule} from "@enterprise-angular/components";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, ComponentsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
